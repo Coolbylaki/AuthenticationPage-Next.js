2d14252554fba57850ff68fd2cbe4b87d8e1caaf
Lazar Stojanović <lakisd98@gmail.com>
2023-08-23 07:56:17 +0200

    Add: font awesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const noto = Noto_Sans({
 	subsets: ["latin"],
@@ -15,7 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={noto.className}>{children}</body>
+			<body className={noto.className} suppressHydrationWarning={true}>
+				{children}
+			</body>
 		</html>
 	);
 }
