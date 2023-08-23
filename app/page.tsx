ee4b22e3f01a7ae924946e2af05b87e7e2f7d300
Lazar Stojanović <lakisd98@gmail.com>
2023-08-24 00:45:58 +0200

    Add: change form button & footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,39 @@
+"use client";
+
 import RegisterForm from "@/components/Forms/RegisterForm";
 import LoginProviders from "@/components/Providers/LoginProviders";
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+	const [showRegister, setShowRegister] = useState(true);
+
 	return (
 		<main className={styles.main}>
 			<Image src="/devchallenges.svg" alt="devChallenges logo" width={130} height={18} />
 
-			<h1 className={styles.title}>Join thousands of learners from around the world </h1>
-			<p className={styles.desc}>
-				Master web development by making real-life projects. There are multiple paths for
-				you to choose
-			</p>
-
-			<RegisterForm />
+			{showRegister && <RegisterForm />}
 
 			<p className={styles.sub__desc}>or continue with these social profile</p>
 
 			<LoginProviders />
+
+			<p className={styles.login}>
+				Already a member?{" "}
+				<span
+					onClick={() => {
+						setShowRegister((prevState) => !prevState);
+					}}>
+					Login
+				</span>
+			</p>
+
+			<footer className={styles.footer}>
+				<p>Lazar Stojanović</p>
+				<p>devChallenges.io</p>
+			</footer>
 		</main>
 	);
 }
--- components/Forms/RegisterForm.tsx
+++ components/Forms/RegisterForm.tsx
@@ -5,25 +5,33 @@ import styles from "./RegisterForm.module.css";
 
 export default function RegisterForm() {
 	return (
-		<form className={styles.reg__form}>
-			<div className={styles.inputContainer}>
-				<FontAwesomeIcon
-					className={styles.ico}
-					icon={faEnvelope}
-					style={{ color: "#828282" }}
-				/>
-				<input type="text" placeholder="Email" />
-			</div>
-			<div className={styles.inputContainer}>
-				<FontAwesomeIcon
-					className={styles.ico}
-					icon={faLock}
-					style={{ color: "#828282" }}
-				/>
-				<input type="text" placeholder="Password" />
-			</div>
+		<>
+			<h1 className={styles.title}>Join thousands of learners from around the world </h1>
+			<p className={styles.desc}>
+				Master web development by making real-life projects. There are multiple paths for
+				you to choose
+			</p>
 
-			<button type="submit">Start coding now</button>
-		</form>
+			<form className={styles.reg__form}>
+				<div className={styles.inputContainer}>
+					<FontAwesomeIcon
+						className={styles.ico}
+						icon={faEnvelope}
+						style={{ color: "#828282" }}
+					/>
+					<input type="text" placeholder="Email" />
+				</div>
+				<div className={styles.inputContainer}>
+					<FontAwesomeIcon
+						className={styles.ico}
+						icon={faLock}
+						style={{ color: "#828282" }}
+					/>
+					<input type="text" placeholder="Password" />
+				</div>
+
+				<button type="submit">Start coding now</button>
+			</form>
+		</>
 	);
 }
